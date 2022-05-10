ed0e7edff8ae986f7001717f442d2cf07153c200
Youness BENNAJ <youness.bennaj@gmail.com>
2022-05-10 19:58:49 +0200

    specify Button component

---
+++ src/components/Button.spec.tsx
@@ -0,0 +1,29 @@
+import { fireEvent, render } from '@testing-library/react'
+import React from 'react'
+
+describe('Button', () => {
+  it('Should render button element with correct wording', () => {
+    const buttonLabel = 'My Button'
+    const { getByRole } = render(<Button>My Button</Button>)
+    const button = getByRole('button', { name: buttonLabel })
+    expect(button).toBeInTheDocument()
+  })
+
+  it('Should have the correct href attribute', () => {
+    const buttonLabel = 'My Button'
+    const { getByRole } = render(<Button href='#portfolio'>My Button</Button>)
+    const button = getByRole('button', { name: buttonLabel })
+    expect(button).toHaveAttribute('href', '#portfolio')
+  })
+
+  it('Should call event handler on click', () => {
+    const buttonLabel = 'My Button'
+    const handleOnClick = jest.fn()
+    const { getByRole } = render(
+      <Button onClick={handleOnClick}>My Button</Button>
+    )
+    const button = getByRole('button', { name: buttonLabel })
+    fireEvent.click(button)
+    expect(handleOnClick).toBeCalled()
+  })
+})
