b029518131914a0e52e75cb4f4e77f9707c0bc0a
Youness BENNAJ <youness.bennaj@gmail.com>
2022-05-10 20:28:34 +0200

    add button story and basic style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 interface ButtonProps {
   children: string | JSX.Element
@@ -6,14 +7,30 @@ interface ButtonProps {
   onClick?: () => void
 }
 
+const ButtonStyled = styled.button`
+  background-color: #203a4c;
+  border: none;
+  padding: 17px 48px;
+
+  a {
+    color: #ffffff;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14px;
+    letter-spacing: 2px;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+`
+
 export const Button: React.FC<ButtonProps> = ({
   children,
   href = '/',
   onClick,
 }) => {
   return (
-    <button onClick={onClick}>
+    <ButtonStyled onClick={onClick}>
       <a href={href}>{children}</a>
-    </button>
+    </ButtonStyled>
   )
 }
