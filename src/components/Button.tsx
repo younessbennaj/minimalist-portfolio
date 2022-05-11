c4c2fed57ba1c6fa2aa3af1838ea229f45940949
Youness BENNAJ <youness.bennaj@gmail.com>
2022-05-11 14:06:58 +0200

    add secondary button and hover states

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
 
+type VariantType = 'primary' | 'secondary'
 interface ButtonProps {
   children: string | JSX.Element
   href?: string
   onClick?: () => void
+  variant?: VariantType
 }
 
-const ButtonStyled = styled.button`
-  background-color: #203a4c;
-  border: none;
-  padding: 17px 48px;
+const ButtonStyled = styled.button<{ variant: VariantType }>`
+  background-color: ${({ variant }) =>
+    variant === 'primary' ? '#203A4C' : 'transparent'};
+  border: ${({ variant }) =>
+    variant === 'primary' ? '1px solid #203A4C' : '1px solid #33323D'};
+  padding: 16px 32px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ variant }) =>
+      variant === 'primary' ? '#5FB4A2;' : '#33323D;'};
+    border: ${({ variant }) =>
+      variant === 'primary' ? '1px solid #5FB4A2;' : '1px solid #33323D;'};
+
+    a {
+      color: #ffffff;
+    }
+  }
 
   a {
-    color: #ffffff;
+    // Css to a wider <a></a> tag clickable area
+    display: inline-block;
+    position: relative;
+    z-index: 1;
+    padding: 16px 32px;
+    margin: -16px -32px;
+    color: ${({ variant }) => (variant === 'primary' ? '#FFFFFF' : '#33323D')};
     font-weight: 400;
     font-size: 12px;
     line-height: 14px;
@@ -25,12 +47,16 @@ const ButtonStyled = styled.button`
 
 export const Button: React.FC<ButtonProps> = ({
   children,
-  href = '/',
+  href,
   onClick,
+  // Primary button by default
+  variant = 'primary',
 }) => {
+  // Conditionnal attribute href
+  const attributes = href ? { href } : {}
   return (
-    <ButtonStyled onClick={onClick}>
-      <a href={href}>{children}</a>
+    <ButtonStyled onClick={onClick} variant={variant}>
+      <a {...attributes}>{children}</a>
     </ButtonStyled>
   )
 }
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -8,3 +8,19 @@ export default {
 }
 
 export const DefaultButton = () => <Button>My Button</Button>
+
+export const PrimaryButton = () => <Button variant='primary'>Primary</Button>
+
+export const PrimaryButtonHover = () => (
+  <Button variant='primary'>Primary</Button>
+)
+PrimaryButtonHover.parameters = { pseudo: { hover: true } }
+
+export const SecondaryButton = () => (
+  <Button variant='secondary'>Secondary</Button>
+)
+
+export const SecondaryButtonHover = () => (
+  <Button variant='secondary'>Secondary</Button>
+)
+SecondaryButtonHover.parameters = { pseudo: { hover: true } }
